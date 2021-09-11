c7b0654af0219e41fed235ff3e638d4fcd24f2b1
ReyML <rey117117@yahoo.com>
2021-09-12 01:23:37 +0400

    sss

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Head>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=0.9" />
         <meta name="description"
           content="Indie maker trying to learn many things at once. I know, I have to focus. My dream is to see this description one day on Google :)" />
         <title>Reyml - Reynol Martinez</title>
